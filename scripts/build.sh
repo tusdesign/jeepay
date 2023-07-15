2775bcc4f7ea71a3e88d5e863f07a5e494fc0dd7
Q.s <wangqs_eclipse@yahoo.com>
2023-07-15 17:45:27 +0800

    fix: parameterize the buuld script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
-docker buildx build . --build-arg PORT=9216 --build-arg PLATFORM=payment -t oci.tuxm.art:8443/tusdesign/jeepay-payment:latest
-docker push oci.tuxm.art:8443/tusdesign/jeepay-payment:latest
-docker buildx build . --build-arg PORT=9217 --build-arg PLATFORM=manager -t oci.tuxm.art:8443/tusdesign/jeepay-manager:latest
-docker push oci.tuxm.art:8443/tusdesign/jeepay-manager:latest
-docker buildx build . --build-arg PORT=9218 --build-arg PLATFORM=merchant -t oci.tuxm.art:8443/tusdesign/jeepay-merchant:latest
-docker push oci.tuxm.art:8443/tusdesign/jeepay-merchant:latest
+docker buildx build . --build-arg PORT=9216 --build-arg PLATFORM=payment -t $HARBOR_URL/tusdesign/jeepay-payment:$TAG
+docker push $HARBOR_URL/tusdesign/jeepay-payment:$TAG
+docker buildx build . --build-arg PORT=9217 --build-arg PLATFORM=manager -t $HARBOR_URL/tusdesign/jeepay-manager:$TAG
+docker push $HARBOR_URL/tusdesign/jeepay-manager:$TAG
+docker buildx build . --build-arg PORT=9218 --build-arg PLATFORM=merchant -t $HARBOR_URL/tusdesign/jeepay-merchant:$TAG
+docker push $HARBOR_URL/tusdesign/jeepay-merchant:$TAG
 
